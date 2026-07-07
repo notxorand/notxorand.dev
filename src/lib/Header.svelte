a81d0946ef7e1a784ebc45d54d2f15837d63d95a
notxorand <ewanretorokugbe@gmail.com>
2026-07-07 04:33:35 +0100

    refactor: move blog to writing

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,18 +1,11 @@
 <script>
 	let title = $state('notxorand');
-
-	function toggleTitle() {
-		title = title === '¬⊕∧' ? 'notxorand' : '¬⊕∧';
-	}
 </script>
 
 <nav class="flex items-center justify-between py-6 pb-8! md:pb-6!">
-	<button onclick={toggleTitle} class="text-lg font-bold no-underline! hover:underline">
-		{title}
-	</button>
+	<a href="/" class="text-lg font-bold no-underline! hover:underline"> notxorand </a>
 	<div class="space-x-1 md:space-x-4">
-		<a href="/" class="text-gray-600 hover:underline">home</a>
-		<a href="/blog" class="text-gray-600 hover:underline">blog</a>
+		<a href="/writing" class="text-gray-600 hover:underline">writing</a>
 		<a href="/philosophy" class="text-gray-600 hover:underline">philosophy</a>
 	</div>
 </nav>
