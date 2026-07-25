8bc05be48566002e5a23dfa18006e1a6cb996535
notxorand <ewanretorokugbe@gmail.com>
2026-07-25 13:23:10 +0100

    chore: favicon
    + fixed missing images from archive post

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.png';
+	import favicon from '$lib/assets/favicon.ico';
 	import Header from '$lib/Header.svelte';
 
 	let { children } = $props();
